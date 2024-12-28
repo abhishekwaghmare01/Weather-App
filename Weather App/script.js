b36225d0b6bfe70cb513c92d6980af6321775d2e
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2024-12-28 17:12:16 +0530

    change API

--- Weather App/script.js
+++ Weather App/script.js
@@ -2,11 +2,12 @@ const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '129d1e3157msh9083d37de0b9690p108bedjsn2700e8b43b71',
-		'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com'
+		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
 
- fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=Chicago&format=json&u=f', options)
+ fetch('https://weatherapi-com.p.rapidapi.com/alerts.json?q=london', options)
 	.then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
+    
